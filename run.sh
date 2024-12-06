a6bb3562797ab946822dac24eea4840f9b48410c
manojl8875 <joelgodat97@gmail.com>
2024-12-06 00:49:29 -0300

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# For more usage info: hellminer --help
+
+# Example SSL URL
+# ./hellminer -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x --cpu 1
+
+./hellminer -c stratum+tcp://sa.vipor.net:5040 -u RUB3hjhynXzcuqBTmaV6bvWXxyWW661V15 -p x --cpu 2
