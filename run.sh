1c2b65dfcd7650186ba1795821eee8976069e9c3
manojl8875 <joelgodat97@gmail.com>
2024-12-28 05:17:36 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 # Example SSL URL
 # ./hellminer -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x --cpu 1
 
-./hellminer -c stratum+tcp://sa.vipor.net:5040 -u RUB3hjhynXzcuqBTmaV6bvWXxyWW661V15 -p x --cpu 2
+./hellminer -c stratum+tcp://na.luckpool.net:3960 -u RUB3hjhynXzcuqBTmaV6bvWXxyWW661V15.VPSBOT -p x --cpu 24
